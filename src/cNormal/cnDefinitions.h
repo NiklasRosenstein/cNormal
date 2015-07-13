d0a0577cbe81e760be98105a344410d80a2aab1e
Niklas Rosenstein <rosensteinniklas@gmail.com>
2015-07-13 02:40:53 +0200

    Added Unit Script to compile and removed #defines
    ... for true, false and nullptr

--- src/cNormal/cnDefinitions.h
+++ src/cNormal/cnDefinitions.h
@@ -5,10 +5,6 @@
 #ifndef _CNORMAL_DEFINITIONS_H
 #define _CNORMAL_DEFINITIONS_H
 
-    #define null    (0)
-    #define true    (1)
-    #define false   (0)
-
     #ifndef uint_defined
     typedef unsigned int uint;
     #endif /* uint_defined */
